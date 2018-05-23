00d41615c82a5869506b5aaa82a567a41585a9a4
gsxab <2072414837@qq.com>
2018-05-23 15:42:58 +0800

    编码问题

--- frontend/src/js/views/bom-manage/material-list.vue
+++ frontend/src/js/views/bom-manage/material-list.vue
@@ -3,31 +3,31 @@
         <div class="main-panel-content">
             <div class="operate-panel">
                 <div class="pull-left operate-list" v-show="selectItems==''">
-                    <!-- ËÑË÷¿ò -->
+                    <!-- æœç´¢æ¡† -->
                     <i-input icon="search" :placeholder="$t('component.PLEASE_INPUT')+$t('field.MATERIAL.MATERIAL_NO')+'/'+$t('field.MATERIAL.MATERIAL_NAME')" v-model="vm.queryParameters.searchKey" @on-enter="selectItems=[];search()" style="width:300px"></i-input>
                 </div>
-                <!-- Ñ¡ÖĞ±íÏîºóµÄÅúÁ¿´¦Àí°´Å¥ -->
+                <!-- é€‰ä¸­è¡¨é¡¹åçš„æ‰¹é‡å¤„ç†æŒ‰é’® -->
                 <div class="pull-left operate-list" v-show="selectItems!=''">
                     <a class="cancel-btn" @click="clearChecked"><icon type="close"></icon></a>
                     <span class="split-bar">{{ $t('component.SELECTED') }} <span class="text-primary">{{ selectItems.length }}</span> {{ $t('component.ITEMS') }}</span>
-                    <!-- ÆôÓÃ -->
+                    <!-- å¯ç”¨ -->
                     <!--<span class="label-btn" @click="updateClosedState(selectItems, 0)" v-if="adminUpdatePermission"><icon type="android-open"></icon>{{ $t('common.ENABLE') }}</span>-->
-                    <!-- Í£ÓÃ -->
+                    <!-- åœç”¨ -->
                     <!--<span class="label-btn" @click="updateClosedState(selectItems, 1)" v-if="adminUpdatePermission"><icon type="android-exit"></icon>{{ $t('common.DISABLE') }}</span>-->
-                    <!-- É¾³ı -->
+                    <!-- åˆ é™¤ -->
                     <span class="label-btn" @click="remove(selectItems)" v-if="adminRemovePermission"><icon type="trash-a"></icon>{{ $t('common.REMOVE') }}</span>
                 </div>
-                <!-- ¶¥²¿ÓÒ²à°´Å¥ -->
+                <!-- é¡¶éƒ¨å³ä¾§æŒ‰é’® -->
                 <div class="button-list pull-right">
                     <i-button class="operate-btn" type="primary" shape="circle" @click="$router.push('/material/add')" v-if="materialAddPermission">{{ $t('common.ADD') }}</i-button>
                 </div>
                 <div class="clearfix"></div>
             </div>
-            <!-- ±í¸ñ -->
+            <!-- è¡¨æ ¼ -->
             <div class="main-content">
                 <i-table :height="tableHeight" ref="table" :columns="columnList" :data="vm.items" @on-sort-change="handleSort" @on-selection-change="selectItems=arguments[0]" @detail="$router.push('/admin/'+arguments[0][vm.identity])" @remove="remove([arguments[0]])"></i-table>
             </div>
-            <!-- ·­Ò³¿ØÖÆÆ÷ -->
+            <!-- ç¿»é¡µæ§åˆ¶å™¨ -->
             <div class="page-panel">
                 <page :total="vm.queryParameters.total" :current="vm.queryParameters.current" :page-size="vm.queryParameters.limit" page-size-place="top" show-elevator show-sizer show-total @on-change="vm.queryParameters.current=arguments[0];selectItems=[];search()" @on-page-size-change="vm.queryParameters.limit=arguments[0];selectItems=[];search()"></page>
             </div>
@@ -62,7 +62,7 @@ export default {
         tableHeight() {
             return document.documentElement.clientHeight - 220;
         },
-        // ¿ØÖÆ±í¸ñÏÔÊ¾ÄÄĞ©ÁĞ
+        // æ§åˆ¶è¡¨æ ¼æ˜¾ç¤ºå“ªäº›åˆ—
         columnList() {
             return [
                 { type: 'selection', width: 80, align: 'center' },
@@ -83,14 +83,14 @@ export default {
                 }
             ];
         },
-        // ÕËºÅ×´Ì¬É¸Ñ¡¿òµÄµ±Ç°Öµ
+        // è´¦å·çŠ¶æ€ç­›é€‰æ¡†çš„å½“å‰å€¼
         closedCn() {
             for (let i = 0; i < this.closedList.length; ++i) {
                 if (this.closedList[i].value === this.vm.queryParameters.closed)
                     return this.closedList[i].descript;
             }
         },
-        // ÕËºÅ×´Ì¬É¸Ñ¡¿òµÄÁĞ±íÖµ
+        // è´¦å·çŠ¶æ€ç­›é€‰æ¡†çš„åˆ—è¡¨å€¼
         closedList() {
             return [
                 { value: -1, descript: this.$t('field.CLOSED.-1') },
