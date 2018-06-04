c3ea9ef5392ac5901f85614858fdf43881114062
faunleaf <faunleaf@aliyun.com>
2018-06-04 10:27:44 +0800

    删除研发领料部分的代码

--- backend/src/main/java/com/springmvc/dto/DrawMaterialBillMaterial.java
+++ backend/src/main/java/com/springmvc/dto/DrawMaterialBillMaterial.java
@@ -3,24 +3,4 @@ package com.springmvc.dto;
 import com.springmvc.pojo.DrawMaterialBillMaterialEntity;
 
 public class DrawMaterialBillMaterial extends DrawMaterialBillMaterialEntity {
-
-    private String materialNo;
-
-    private String materialName;
-
-    public String getMaterialNo() {
-        return materialNo;
-    }
-
-    public void setMaterialNo(String materialNo) {
-        this.materialNo = materialNo;
-    }
-
-    public String getMaterialName() {
-        return materialName;
-    }
-
-    public void setMaterialName(String materialName) {
-        this.materialName = materialName;
-    }
 }
--- frontend/src/js/service/development-draw.js
+++ frontend/src/js/service/development-draw.js
@@ -1,19 +1,5 @@
 import { http } from '../libs/http';
 
-var getBill = (billId) => http.post('development-draw/billDetail', { billId: billId });
 
-var searchBill = (queryParameters) => http.post('development-draw/searchBill', queryParameters);
 
-var auditBill = (idList) => http.post('development-draw/auditBill', { idList: idList });
-
-var unauditBill = (idList) => http.post('development-draw/unauditBill', { idList: idList });
-
-var getMaterialList = () => http.post('development-draw/getMaterialList');
-
-var addBill = (bill) => http.post('development-draw/addBill', bill)
-
-var updateBill = (bill) => http.post('development-draw/updateBill', bill)
-
-var deleteBill = (idList) => http.post('development-draw/deleteBill', { idList: idList });
-
-export default { getBill, searchBill, auditBill, unauditBill, getMaterialList, addBill, updateBill, deleteBill };
+export default {  };
